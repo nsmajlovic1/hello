8154681533b98f79cca50b8640d18210105b9172
Nordin Smajlovic <nsmajlovic1@etf.unsa.ba>
2021-10-18 17:49:05 +0200

    Promijenjen tekst

--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -3,6 +3,6 @@ package ba.unsa.etf.rpr;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Dobro dosli na RPR");
+        System.out.println("Nordin");
     }
 }
